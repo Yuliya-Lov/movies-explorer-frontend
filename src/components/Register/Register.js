df1d15162d784909c9e5178004bdca98c29df61a
Yuliya-Lov <jul-yaris@yandex.ru>
2023-09-08 15:10:25 +0300

    feat: signup

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Register.css';
+import EntryForm from '../EntryForm/EntryForm';
 
 function Register(props) {
   return (
-    <div>
-
-    </div>
+    <section className='register'>
+      <h2 className='register__greeting'>Добро пожаловать!</h2>
+      <EntryForm buttonText='Зарегистрироваться' />
+      <p className='register__redirection'>Уже зарегистрированы?<Link className='register__link' to='/signin'>Войти</Link></p>
+    </section>
   );
 }
 
