77c195fe74c2a1a4ad6c66b25c89a32e42cdc4c0
Yuliya-Lov <jul-yaris@yandex.ru>
2023-09-08 09:21:31 +0300

    feat: active menu links

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
-import { Link } from 'react-router-dom';
 import Header from '../Header/Header.js';
 import Main from '../Main/Main.js';
 import Movies from '../Movies/Movies.js';
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Navigation.css';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navigation({ isMobile }) {
+  const location = useLocation();
+
+  const linkClass = isMobile
+    ? 'navigation__link navigation__link_mobile'
+    : 'navigation__link';
+  const activeLinkClass = isMobile
+    ? 'navigation__link_mobile-active'
+    : 'navigation__link_active';
+
+
   return (
     <nav className={`navigation ${isMobile && 'navigation_mobile'}`}>
       <div className={`navigation__movies ${isMobile && 'navigation__movies_mobile'}`}>
-        {isMobile && <Link className={`navigation__link ${isMobile && 'navigation__link_mobile'}`} to="/">Главная</Link>}
-        <Link className={`navigation__link ${isMobile && 'navigation__link_mobile'}`} to="/movies">Фильмы</Link>
-        <Link className={`navigation__link navigation__link_active ${isMobile && 'navigation__link_mobile navigation__link_mobile-active'}`} to="/saved-movies">Сохранённые фильмы</Link>
+        {isMobile && <Link className={`${linkClass} ${location.pathname === '/' && activeLinkClass}`} to="/">Главная</Link>}
+        <Link  className={`${linkClass} ${location.pathname === '/movies' && activeLinkClass}`} to="/movies">Фильмы</Link>
+        <Link  className={`${linkClass} ${location.pathname === '/saved-movies' && activeLinkClass}`} to="/saved-movies">Сохранённые фильмы</Link>
       </div>
       <Link className='navigation__profile-link' to="/profile">Аккаунт</Link>
     </nav>
